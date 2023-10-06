462df0c48b784f0512c9676a9429a208bfab70fb
Debosmita Chatterjee <145691289+DebosmitaChatterjee24@users.noreply.github.com>
2023-10-06 20:06:26 +0530

    Create 1855. Maximum Distance Between a Pair of Values.cpp

---
+++ Solutions/1855. Maximum Distance Between a Pair of Values.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0, j = 0, ans = 0;
+    while(i < nums1.size() && j < nums2.size()){
+        if(nums1[i] > nums2[j])
+            i++;
+        else{
+            ans = max(ans, j-i);
+            j++;
+        }
+    }
+    return ans;
+    }
+};
